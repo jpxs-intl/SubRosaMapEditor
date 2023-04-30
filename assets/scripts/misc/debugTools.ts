b0f56235676296c9ef81b6a7bc79d8e016547c86
checkraisefold <devsmash1@gmail.com>
2023-04-29 20:46:44 -0700

    IT'S NOT SHUTTING DOWN!!!!

--- assets/scripts/misc/debugTools.ts
+++ assets/scripts/misc/debugTools.ts
@@ -11,6 +11,105 @@ import { BlockFile } from "../typings/blockFile";
 export default class DebugTools {
   public static init() {}
 
+  public static renderBlock(block: BlockFile, adjustVec: THREE.Vector3, blockName: string) {
+    /*for (const surface of block.surfaces) {
+      for (let surfL = 0; surfL < 4; surfL++) {
+        for (let surfW = 0; surfW < 4; surfW++) {                    
+          const surfaceData = surface.data[surfL][surfW];
+          const vertexMarker = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+          const vertexMarkerMaterial = new THREE.MeshBasicMaterial({
+            color: 0x00ff00,
+          });
+          const vertexMarkerMesh = new THREE.Mesh(
+            vertexMarker,
+            vertexMarkerMaterial
+          );
+          vertexMarkerMesh.position.set(
+            buildingData.position[0] +
+              surfaceData.vertex[0] +
+              l,
+            buildingData.position[1] +
+              surfaceData.vertex[1] +
+              h,
+            buildingData.position[2] +
+              surfaceData.vertex[2] +
+              w
+          );
+          Main.scene.add(vertexMarkerMesh);
+        }
+      }
+    }*/
+    for (const box of block.boxes) {
+      let highestVertex = [-Infinity, -Infinity, -Infinity];
+      let lowestVertex = [Infinity, Infinity, Infinity];
+      for (const vertex of box.vertices) {
+        if (
+          vertex[0] > highestVertex[0] ||
+          vertex[1] > highestVertex[1] ||
+          vertex[2] > highestVertex[2]
+        ) {
+          highestVertex = [...vertex];
+        }
+        if (
+          vertex[0] < lowestVertex[0] ||
+          vertex[1] < lowestVertex[1] ||
+          vertex[2] < lowestVertex[2]
+        ) {
+          lowestVertex = [...vertex];
+        }
+      }
+
+      let minVec = new THREE.Vector3(
+        lowestVertex[0],
+        lowestVertex[1],
+        lowestVertex[2]
+      );
+      let maxVec = new THREE.Vector3(
+        highestVertex[0],
+        highestVertex[1],
+        highestVertex[2]
+      );
+      let dimensions = new THREE.Vector3().subVectors(maxVec, minVec);
+
+      //if(tile.buildBlock.search("stairwell") != -1)
+      //console.log(maxVec, minVec, tile.buildBlock)
+
+      const boxGeo = new THREE.BoxGeometry(
+        dimensions.x,
+        dimensions.y,
+        dimensions.z
+      );
+      const matrix = new THREE.Matrix4().setPosition(
+        dimensions.addVectors(minVec, maxVec).multiplyScalar(0.5).add(adjustVec)
+      );
+
+      boxGeo.applyMatrix4(matrix);
+      const material = new THREE.MeshBasicMaterial({
+        color: Utils.getColorFromString(blockName),
+      });
+      const mesh = new THREE.Mesh(boxGeo, material);
+
+      Main.scene.add(mesh);
+
+      /*const buildName = new TextGeometry(tile.buildBlock, {
+        font: Main.font,
+        size: 1,
+        height: 0.1,
+        curveSegments: 12,
+        bevelEnabled: false,
+      });
+
+      const buildNameMaterial = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+      });
+      const buildNameMesh = new THREE.Mesh(buildName, buildNameMaterial);
+
+      buildNameMesh.position.copy(maxVec.add(adjustVec).add(new THREE.Vector3(0, 5, 0)))
+      Main.scene.add(buildNameMesh);
+    }*/
+    }
+  }
+
   public static renderDebugCityInfo(cityData: CityFile) {
     // render intersections
     cityData.intersections.forEach((intersection, index) => {
@@ -90,7 +189,7 @@ export default class DebugTools {
 
     // render buildings
 
-    for(let buildingData of cityData.buildings) {
+    for (let buildingData of cityData.buildings) {
       const building = BuildingManager.instance.getBuilding(buildingData.name);
       if (!building)
         throw new Error(`Building ${buildingData.name} not found!`);
@@ -102,103 +201,39 @@ export default class DebugTools {
             if (!tile) continue;
 
             if (tile.buildBlock || tile.interiorBlock || tile.block) {
-              let block: BlockFile | undefined = undefined
+              let block: BlockFile | undefined = undefined;
               let interiorBlock: BlockFile | undefined = undefined;
               let buildBlock: BlockFile | undefined = undefined;
 
-              if(typeof tile.block != "number" && tile.block)
+              let adjustVec = new THREE.Vector3(
+                buildingData.position[0] + l,
+                buildingData.position[1] + h,
+                buildingData.position[2] + w
+              );
+
+              if (typeof tile.block != "number" && tile.block)
                 block = BlockManager.instance.getblock(tile.block);
-              if(typeof tile.interiorBlock != "number" && tile.interiorBlock)
+              if (typeof tile.interiorBlock != "number" && tile.interiorBlock)
                 interiorBlock = BlockManager.instance.getblock(tile.interiorBlock);
-              if(tile.buildBlock)
+              if (tile.buildBlock)
                 buildBlock = BlockManager.instance.getblock(tile.buildBlock);
 
-              if (!block && !buildBlock && !interiorBlock) {
+              if(block)
+                this.renderBlock(block, adjustVec, tile.block as string)
+              if(interiorBlock)
+                this.renderBlock(interiorBlock, adjustVec, tile.interiorBlock as string)
+              if(buildBlock)
+                this.renderBlock(buildBlock, adjustVec, tile.buildBlock as string)
+
+              /*if (!block && !buildBlock && !interiorBlock) {
                 //throw new Error(`Block ${tile.buildBlock} not found!`)
                 // console.log("funny not there", tile.buildBlock);
                 continue;
-              }
+              }*/
 
-              console.log(tile.block, tile.buildBlock, tile.interiorBlock, block, buildBlock, interiorBlock)
               // console.log(block.floor, block.ceiling, block.wall1, block.wall2, block.wall3, block.wall4, tile.buildBlock, block.size0, block.size1, block.size2)
 
               //console.log(tile.buildBlock, block.surfaces.length)
-              /*
-             for (const surface of block.surfaces) {
-                for (let surfL = 0; surfL < 4; surfL++) {
-                  for (let surfW = 0; surfW < 4; surfW++) {                    
-                    const surfaceData = surface.data[surfL][surfW];
-                    const vertexMarker = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-                    const vertexMarkerMaterial = new THREE.MeshBasicMaterial({
-                      color: 0x00ff00,
-                    });
-                    const vertexMarkerMesh = new THREE.Mesh(
-                      vertexMarker,
-                      vertexMarkerMaterial
-                    );
-                    vertexMarkerMesh.position.set(
-                      buildingData.position[0] +
-                        surfaceData.vertex[0] +
-                        l,
-                      buildingData.position[1] +
-                        surfaceData.vertex[1] +
-                        h,
-                      buildingData.position[2] +
-                        surfaceData.vertex[2] +
-                        w
-                    );
-                    Main.scene.add(vertexMarkerMesh);
-                  }
-                }
-              }
-              for (const box of block.boxes) {
-                let highestVertex = [-Infinity, -Infinity, -Infinity]
-                let lowestVertex = [Infinity, Infinity, Infinity]
-                for(const vertex of box.vertices) {
-                  if (vertex[0] > highestVertex[0] || vertex[1] > highestVertex[1] || vertex[2] > highestVertex[2]) {
-                    highestVertex = [...vertex]
-                  }
-                  if (vertex[0] < lowestVertex[0] || vertex[1] < lowestVertex[1] || vertex[2] < lowestVertex[2]) {
-                    lowestVertex = [...vertex]
-                  }
-                }
-
-                let adjustVec = new THREE.Vector3(buildingData.position[0] + l, buildingData.position[1] + h, buildingData.position[2] + w)
-                let minVec = new THREE.Vector3(lowestVertex[0], lowestVertex[1], lowestVertex[2])
-                let maxVec = new THREE.Vector3(highestVertex[0], highestVertex[1], highestVertex[2])
-                let dimensions = new THREE.Vector3().subVectors( maxVec, minVec );
-                
-                //if(tile.buildBlock.search("stairwell") != -1)
-                  //console.log(maxVec, minVec, tile.buildBlock)
-                
-
-                const boxGeo = new THREE.BoxGeometry(dimensions.x, dimensions.y, dimensions.z);
-                const matrix = new THREE.Matrix4().setPosition(dimensions.addVectors(minVec, maxVec).multiplyScalar( 0.5 ).add(adjustVec));
-                
-                boxGeo.applyMatrix4(matrix)
-                const material = new THREE.MeshBasicMaterial({
-                  color: Utils.getColorFromString(tile.buildBlock),
-                });
-                const mesh = new THREE.Mesh(boxGeo, material);
-
-                Main.scene.add(mesh);
-
-                /*const buildName = new TextGeometry(tile.buildBlock, {
-                  font: Main.font,
-                  size: 1,
-                  height: 0.1,
-                  curveSegments: 12,
-                  bevelEnabled: false,
-                });
-          
-                const buildNameMaterial = new THREE.MeshBasicMaterial({
-                  color: 0xff0000,
-                });
-                const buildNameMesh = new THREE.Mesh(buildName, buildNameMaterial);
-          
-                buildNameMesh.position.copy(maxVec.add(adjustVec).add(new THREE.Vector3(0, 5, 0)))
-                Main.scene.add(buildNameMesh);
-              }*/
             }
           }
         }
@@ -217,6 +252,6 @@ export default class DebugTools {
       mesh.scale.set(building.width, building.height, building.length);
 
       Main.scene.add(mesh);*/
-    };
+    }
   }
 }
